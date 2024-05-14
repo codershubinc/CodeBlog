71b379e006215c8aae5d91d7464606a944496efe
Swapnil ingle <90494823+CodersHub-in@users.noreply.github.com>
2024-05-14 15:09:05 +0530

    Update GetByUrl.jsx

--- src/components/pages/GetByUrl.jsx
+++ src/components/pages/GetByUrl.jsx
@@ -4,7 +4,9 @@ function GetByUrl() {
     const urlParams = new URLSearchParams(window.location.search);
     const secret = urlParams.get('secret');
     const userId = urlParams.get('userId');
-    console.log('GetByUrl ::', urlParams, 'secret ::', secret, ' userId ::', userId);
+    console.log('GetByUrl ::'urlParams);
+    console.log( 'secret ::', secret)
+    console.log(' userId ::', userId)
 
 
     return (
